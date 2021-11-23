196b504718dd61b3b2beba69f310dbe925ac3a60
shovaaa <shova.723@gmail.com>
2021-11-23 11:32:12 +1100

    Made common button component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-function App() {
+import React from "react";
+import Button from "./blocks/Button/Button";
+import '../assets/styles/theme/app.scss';
+
+const App = () => {
   return (
     <div className="App">
       <p>Hello!</p>
+      <Button variant="blue" buttonText="Save" buttonLink="#" buttonAlt="click" size="sm" />
+      <Button variant="outline-grey" buttonText="Previous Page" buttonLink="#" buttonAlt="click" size="sm" />
     </div>
   );
 }
