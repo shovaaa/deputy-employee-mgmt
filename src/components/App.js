4b9e432bf7c6a76b06ab302f01e7c7dae7af5534
shovaaa <shova.723@gmail.com>
2021-11-23 16:46:55 +1100

    Made common input box component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "./blocks/Button/Button";
 import '../assets/styles/theme/app.scss';
+import InputBox from "./blocks/InputBox/InputBox";
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <p>Hello!</p>
       <Button variant="blue" buttonText="Save" buttonLink="#" buttonAlt="click" size="sm" />
       <Button variant="outline-grey" buttonText="Previous Page" buttonLink="#" buttonAlt="click" size="sm" />
+      <div>
+        <InputBox label="Name" name="employee-name" id="employee-name" type="text" minLength={1} maxLength={150} displayHeight="sm" displayWidth="w-50"/>
+      </div>
     </div>
   );
 }
---
+++ src/interfaces/InputBox.ts
@@ -0,0 +1,5 @@
+export interface IInput extends HTMLInputElement {
+  label: string;
+  displayHeight: string;
+  displayWidth: string;
+}
